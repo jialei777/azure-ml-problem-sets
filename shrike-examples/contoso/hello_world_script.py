34f5f390f5df632053d4b95a0641d274adaabdcf
jialei777 <jialeichen@microsoft.com>
2021-12-21 10:17:37 -0800

    print date and time in pipeline 1 demo

--- shrike-examples/contoso/hello_world_script.py
+++ shrike-examples/contoso/hello_world_script.py
@@ -1,6 +1,12 @@
+import datetime
+
+
 def main():
     """The main function"""
-    print("Hello, world!")
+    now = datetime.datetime.now()
+
+    print("Hello, world from Jialei!")
+    print("   -- Current date and time: ", str(now)) # print current date and time
 
 if __name__ == "__main__":
     main()
