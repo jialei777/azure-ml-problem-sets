91a63a95df6e79ec7a3fd9cf5a4a4835c16be5b3
jialei777 <jialeichen@microsoft.com>
2021-12-22 09:47:49 -0800

    finish pipeline 7

--- shrike-examples/tests/test_pipelines.py
+++ shrike-examples/tests/test_pipelines.py
@@ -5,7 +5,9 @@ PyTest suite for testing all runnable pipelines.
 import sys
 from unittest.mock import patch
 
-# To-Do: import your pipeline class
+# import pipeline class
+#from pipelines.experiments.demo_subgraph import DemoGraphWithSubgraph
+from pipelines.experiments.demo_dynamic_graph import DynamicGraphDemo
 
 ### Pipeline validation tests (integration tests)
 
@@ -16,9 +18,12 @@ def test_demo_subgraph_build_local(pipeline_config_path="pipelines/config"):
         "--config-dir",
         pipeline_config_path,
         "--config-name",
-        "experiments/<YourExperimentConfigFile>", # To-Do: point to the right config file
-        "module_loader.use_local='<ComponentKey>'", # To-Do: make sure the local version is used for ALL components - '*' could be helpful here
+        # "experiments/demo_subgraph", # To-Do: point to the right config file
+        "experiments/demo_dynamic_graph",
+        "module_loader.use_local='*'", # To-Do: make sure the local version is used for ALL components - '*' could be helpful here
     ]
     # will do everything except submit :)
     with patch.object(sys, "argv", testargs):
+        #DemoGraphWithSubgraph.main()
+        DynamicGraphDemo.main()
         # To-Do: call the main method of your pipeline
