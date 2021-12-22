0bf18b42a06b1ee9dd500358bce775b9c4f71954
jialei777 <jialeichen@microsoft.com>
2021-12-22 14:04:33 -0800

    finish logging 3

--- shrike-examples/contoso/log_stack_trace_prefixing_script.py
+++ shrike-examples/contoso/log_stack_trace_prefixing_script.py
@@ -1,32 +1,48 @@
 # To-Do: import relevant libraries
+from shrike.compliant_logging.exceptions import prefix_stack_trace 
 
+
+
+def try_a_error():
+    print(1 / 0)
 
 # To-Do: decorator with custom prefix
+@prefix_stack_trace(prefix="MyCustomPrefix")
 def custom_prefix():
     print(1 / 0)
 
 
 # To-Do: decorator with custom message
+@prefix_stack_trace(scrub_message="Private data was divided by zero")
 def custom_message():
     print(1 / 0)
 
 
 # To-Do: decorator with exception message displayed
+@prefix_stack_trace(keep_message=True)
 def keep_exception_message():
     print(1 / 0)
 
 
 # To-Do: decorator with allow-list for exceptions
+@prefix_stack_trace(keep_message=False, allow_list=["ZeroDivision"])
 def keep_allowed_exceptions():
     print(1 / 0)
 
 
 # To-Do: decorator with timestamp displayed in logs
+@prefix_stack_trace(add_timestamp=True)
 def add_timestamp():
     print(1 / 0)
 
 
 def main():
+    try:
+        print("try -- no decorator")
+        try_a_error()
+    except:
+        print("expect -- no decorator")
+        pass
     try:
         custom_prefix()
     except:
